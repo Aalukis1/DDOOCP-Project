5b9d0c939abc32eeb3bf9daf45b2917fe31d7ba2
Lukeman <Lukeman@DESKTOP-380GS09>
2023-06-07 13:55:03 +0100

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿//Program starts here
+/* This is to print Hello World
+ using console
+ 
+ */
+Console.WriteLine("Hello, David!");
+
+
+
+//Declaration of Variables
+string studentName;
+int studentAge;
+bool studentPresent;
+char studentSex;
+double studentFee;
+
+//Initialzing or assigning values to the variables
+//studentName = "David";
+studentAge = 13;
+//studentPresent = true;
+//studentSex = 'M';
+//studentFee = 1000.60;
+
+//Basic operations with the variables (+,-,*,/,%)
+//Addition
+studentAge += 23;
+Console.WriteLine("Your new age is " + studentAge + " Years");
+
+Console.ReadLine();
